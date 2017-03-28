a19fdaea03edaee2db8338fae3ffb9d964c52276
RoydCapella <roydcapella@gmail.com>
2017-03-28 02:25:28 -0300

    render

--- src/app/diseno/citas/citas.component.ts
+++ src/app/diseno/citas/citas.component.ts
@@ -1,6 +1,44 @@
+/*import components*/
 import { Component, OnInit } from '@angular/core';
 
+/*import services*/
 import { CitasService } from './citas.service';
+
+
+let citas2 = {
+lunes: [
+{nombre: 'Daniel', hora_inicio: '08:00', hora_termino: '09:00'},
+{nombre: 'Daniel', hora_inicio: '09:30', hora_termino: '11:00'},
+{nombre: 'Daniel', hora_inicio: '15:00', hora_termino: '16:00'},
+{nombre: 'Daniel', hora_inicio: '17:00', hora_termino: '19:30'}
+],
+martes: [
+{nombre: 'Daniel', hora_inicio: '08:00', hora_termino: '09:00'},
+{nombre: 'Daniel', hora_inicio: '11:30', hora_termino: '12:00'},
+{nombre: 'Daniel', hora_inicio: '15:00', hora_termino: '16:00'},
+{nombre: 'Daniel', hora_inicio: '17:00', hora_termino: '19:30'}
+],
+miercoles: [
+{nombre: 'Daniel', hora_inicio: '08:00', hora_termino: '09:00'},
+{nombre: 'Daniel', hora_inicio: '10:30', hora_termino: '12:00'},
+{nombre: 'Daniel', hora_inicio: '15:00', hora_termino: '16:00'},
+{nombre: 'Daniel', hora_inicio: '17:00', hora_termino: '19:30'}
+],
+jueves: [
+{nombre: 'Daniel', hora_inicio: '08:00', hora_termino: '09:00'},
+{nombre: 'Daniel', hora_inicio: '09:30', hora_termino: '12:00'},
+{nombre: 'Daniel', hora_inicio: '15:00', hora_termino: '16:00'},
+{nombre: 'Daniel', hora_inicio: '17:00', hora_termino: '19:30'}
+],
+viernes: [
+
+{nombre: 'Daniel', hora_inicio: '08:00', hora_termino: '09:00'},
+{nombre: 'Daniel', hora_inicio: '09:30', hora_termino: '12:00'},
+{nombre: 'Daniel', hora_inicio: '15:00', hora_termino: '16:00'},
+{nombre: 'Daniel', hora_inicio: '17:00', hora_termino: '19:30'}
+],
+};
+
 
 @Component({
   selector: 'app-citas',
@@ -10,74 +48,91 @@ import { CitasService } from './citas.service';
 })
 
 export class CitasComponent implements OnInit {
+  private tablaCitas: string = "";
+  private contenido:string="";
   private citas;
+  private intervalos;
 
-  constructor(private citasService: CitasService) { }
+  constructor(private citasService: CitasService) {
 
-  ngOnInit() {
-    this.renderCitas()
   }
 
-  renderCitas(){
-    let nombreColumna;
-    let intervalos= this.getIntervalos("08:00", "19:30",30);
+
+  ngOnInit() {
+    this.getCitas();
+    //this.tablaCitas = this.renderCitas();
+  }
+
+getIntervalos(){
+  return this.citasService.getIntervalos("08:00", "19:30", 30).then(intervalos => {
+    this.intervalos = intervalos;
+  });
+}
+/*
+return this.citasService.getIntervalos("08:00", "19:30", 30).then(intervalos => {
+  this.intervalos = intervalos;
+
+});
+*/
+  getCitas() {
+
+    return this.citasService.get().then(citas => {
+      return this.citasService.getIntervalos("08:00", "19:30", 30).then(intervalos => {
+        this.intervalos = intervalos;
+        this.citas = citas;
+        this.contenido=this.renderCitas();
+      });
+    });
+  }
+
+
+  renderCitas() {
+    let nombreColumna: string;
+    let listColumnas = ["hora", "lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo", "hora"];
     let html = "<div class='row table'>";
-    for(let columna:number=0; columna <=9 ; columna ++){
-      nombreColumna= this.getNombreColumna(0);
-      if (columna==0||columna == 9){
-        html += "<div class='col-md-1 columnaHora' id='columna"+columna+"'>";
-        html += "<h3 class='title'>"+ nombreColumna +"</h3>";
-        for (let intervalo = 0;intervalo<= intervalos.length; intervalo++){
+    for (let columna: number = 0; columna < 9; columna++) {
+      nombreColumna = listColumnas[columna];
+      if (columna == 0 || columna == 8) {
+        html += "<div class='col-md-1 columnaHora' id='columna" + columna + "'>";
+        html += "<h4 class='title'>" + nombreColumna + "</h4>";
+
+        for (let intervalo = 0; intervalo < this.intervalos.length; intervalo++) {
           html += "<div class='bloque hora' >"
-          html += "<span class='hora'>"+ intervalos[intervalo] + "</span>";
+          html += "<span class='hora'>" + this.intervalos[intervalo] + "</span>";
           html += "</div>";
         }
-        html="</div>";
-      }else{
-        html += "<div class='col-md-1 columnaDia' id='columna"+columna+"'>";
-        html += "<h3 class='title'>"+ nombreColumna +"</h3>";
+        html += "</div>";
+      } else {
+        html += "<div class='col-md-1 columnaDia' id='columna" + columna + "'>";
+        html += "<h4 class='title'>" + nombreColumna + "</h4>";
 
-        let citasDia = citas[nombreColumna];
+        let citasDia = this.citas[nombreColumna];
+        console.log(this.citas)
 
-        if (citasDia.length>0){
 
-          for (let hora=0; hora<= intervalos.length; hora++){
-            citaActual = citasDia[0];
-            if (citaActual !=null){
-              if (citasActual.hora_inicio== intervalos[hora]){
-                citaActual.hora_inicio = citaActual.hora_inicio+30; ///funcion que sume 30 a hora_inicio
-                if (citaActual.hora_termino <= citaActual.hora_inicio){
+        for (let hora = 0; hora <= this.intervalos.length; hora++) {
+          if (citasDia!=null && citasDia.length > 0) {
+            let citaActual = citasDia[0];
+            if (citaActual != null) {
+              if (citaActual.hora_inicio == this.intervalos[hora]) {
+                citaActual.hora_inicio = citaActual.hora_inicio + 30; ///funcion que sume 30 a hora_inicio
+                if (citaActual.hora_termino <= citaActual.hora_inicio) {
                   html += "<div class='bloque cita' >"
-                  html += "<span class='cita'>"+ citaActual.nombre + "</span>";
+                  html += "<span class='cita'>" + citaActual.nombre + "</span>";
                   html += "</div>";
                   citasDia.shift();
                 }
               }
             }
+          } else {
+            html += "<div class='bloque bloque-vacio'><div>";
           }
-        }else{
-          html="<div class='sin-contenido'><div>";
         }
-
-
+        html += "</div>";
       }
-
-
-      }
-
-
-
     }
-
-
-
-    return this.citasService.get().then(citas => {
-
-
-      this.citas = citas;
-
-    });
+    html += "</div>"; //cierra tabla-
+    console.log(html)
+    return html;
   }
-
-
 }
--- src/app/diseno/citas/citas.service.ts
+++ src/app/diseno/citas/citas.service.ts
@@ -46,17 +46,19 @@ export class CitasService {
 
   get(){
     return new Promise(resolve =>{
-      console.log("resolve"+resolve)
-       resolve(this.retrieveCitas())
+      console.log("Citas:")
+      console.log(citas)
+       resolve(citas);
     });
   }
 
-   buildIntervalDay(horaInicial, horaFinal, intervalo){
+  getIntervalos(horaInicial, horaFinal, intervalo){
+    return new Promise(resolve =>{
+     let array = new Array();
      let v1 = horaInicial.split(':');
      let v2 = horaFinal.split(':');
      let momentB = new Date(this.dateBuilder);
      let momentA = new Date(this.dateBuilder);
-     let array = new Array();
      let hour:number;
      let minutes:number;
 
@@ -67,61 +69,15 @@ export class CitasService {
        hour =momentA.getHours();
        minutes= momentA.getMinutes();
        array.push(((hour<10)? '0'+hour: hour) +':'+((minutes<10)? '0'+minutes: minutes));
-       momentA.setSeconds(intervalo); //Añado el tiempo
+       momentA.setSeconds(30*60); //Añado el tiempo
      }
+     console.log("Intervalos:")
      console.log(array)
-     return array;
-   }
+     resolve(array);
+   });
+  }
 
-   retrieveCitas(){
-     let day = ["lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo"];
-     let intervalos = this.buildIntervalDay('08:00', '19:30', 30*60);
-     let dateComparer = new Date(this.dateBuilder);
-     let container = new Array(intervalos.length-1);
-
-     /*Recorremos los intervalos por cada dia*/
-     for(var i: number = 0; i <= intervalos.length; i++) {
-                container[i]=new Array(7);
-
-                /*Recorremos los dias de la semana*/
-                 for(var j: number = 0; j< 7; j++) {
-                   let citasHoy = citas[day[j]];
-                   if (citasHoy!=null){
-                     if(citasHoy.length>0){
-                       let value ="";
-                       let cita = citasHoy[0];
-
-
-                      // let hm  = cita.hora_inicio.split(':')
-                      // dateComparer.setHours(hm[0], hm[1])
-                      // console.log("date" + dateComparer)
-                      //  console.log("nterval" + intervalos[i])
-                       if (cita.hora_inicio == intervalos[i]){
-                         let mi= intervalos[i++];
-                         if (mi!=null){
-
-                           console.log(cita)
-                           cita.hora_inicio = mi;
-                           console.log(cita)
-                           console.log(mi)
-                           //dateComparer.setSeconds(30*60);
-                           //cita.hora_inicio = dateComparer.getHours()+':'+dateComparer.getMinutes();
-                           if (cita.hora_inicio >=cita.hora_termino)
-                               value= citasHoy.shift();
-                           console.log("si" + i +j )
-                            container[i][j]="as";
-                         }
-
-                       }
-                      //  console.log("no")
-                     }
-                   }
-                 }
-             }
-             return container;
-   }
 }
-
 /*
 export class Cita{
 public nombre:string;
